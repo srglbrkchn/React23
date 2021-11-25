f047d3ca58fb4052cc623cb81900b1264e32595e
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-25 18:31:39 +0100

    Render comfirm password only if the condition is false usein && operator. Complete challange. Final commit.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,7 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+      {!registered && <input type="password" placeholder="Confirm Password" />}
       <button type="submit">{registered ? "Login" : "Register"} </button>
     </form>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
